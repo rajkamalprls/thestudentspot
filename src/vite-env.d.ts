42a6fa8f76b0d4165df8472071a4bf300412da7c
RAJKAMAL PANTHAGANI <115925450+rajkamalprls@users.noreply.github.com>
2025-06-03 18:20:27 +0530

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,13 @@
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['firebase/auth'] // 👈 Tells Rollup to skip bundling this
+    }
+  }
+});
 /// <reference types="vite/client" />
