59d77688a043d95c4a94ab2da6aed5d4cef6260f
RAJKAMAL PANTHAGANI <115925450+rajkamalprls@users.noreply.github.com>
2025-06-03 18:32:10 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
 import React, { useEffect } from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useLocation
-} from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 
-// Layout Components
+// Components
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
 
 // Pages
 import Home from './pages/Home';
 import About from './pages/About';
-import Contact from './pages/Contact';
-import Events from './pages/Events';
 import JobOpportunities from './pages/JobOpportunities';
-import Login from './pages/Login';
 import Resources from './pages/Resources';
+import Events from './pages/Events';
+import Contact from './pages/Contact';
 import NotFound from './pages/NotFound';
 
 // Service Pages
@@ -25,44 +19,37 @@ import CareerGuidance from './pages/services/CareerGuidance';
 import ResumeBuilding from './pages/services/ResumeBuilding';
 import Mentorship from './pages/services/Mentorship';
 
-// Scroll-to-top on route change
-const ScrollToTop = () => {
-  const { pathname } = useLocation();
+function App() {
+  const location = useLocation();
+
+  // Scroll to top on route change
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, [pathname]);
-  return null;
-};
+  }, [location.pathname]);
 
-const App = () => {
   return (
-    <Router>
-      <ScrollToTop />
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/events" element={<Events />} />
-            <Route path="/jobs" element={<JobOpportunities />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/resources" element={<Resources />} />
-
-            {/* Service Routes */}
-            <Route path="/services/career-guidance" element={<CareerGuidance />} />
-            <Route path="/services/resume-building" element={<ResumeBuilding />} />
-            <Route path="/services/mentorship" element={<Mentorship />} />
-
-            {/* Catch-all for 404 */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/jobs" element={<JobOpportunities />} />
+          <Route path="/resources" element={<Resources />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/contact" element={<Contact />} />
+          
+          {/* Service Routes */}
+          <Route path="/services/career-guidance" element={<CareerGuidance />} />
+          <Route path="/services/resume-building" element={<ResumeBuilding />} />
+          <Route path="/services/mentorship" element={<Mentorship />} />
+          
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   );
-};
+}
 
 export default App;
